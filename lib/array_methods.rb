fba3ba9148f3b682489c54ab67b9117512c8f91c
Mustafa Muse <umpp101@icloud.com>
2019-09-12 18:05:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  puts find_element_index.find_element_index["value_to_find"]
 end
 
 def find_max_value(array)
-  # Add your solution here
+  puts array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  puts array.min
 end
