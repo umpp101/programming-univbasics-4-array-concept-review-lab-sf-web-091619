806749ec8d2fdc40c7ce8e5e27bd44bfc8e7e038
Mustafa Muse <umpp101@icloud.com>
2019-09-12 18:22:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
 def find_element_index(array, value_to_find)
-  puts find_element_index.find_element_index["value_to_find"]
+  i = 0 
+  while i < array.length do
+    array.index(value_to_find[i])
+  end
 end
 
 def find_max_value(array)
